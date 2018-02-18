9182963880b7200501ec4af91f1203da0598e9a8
samuelfiori <samuel.fiori1@gmx.de>
2018-02-18 14:26:53 +0000

    step_10_error_handling

--- movies/urls.py
+++ movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-    path('create/', views.create, name='create'),
-    path('edit/<str:movie_id>', views.edit, name='edit'),
-    path('delete/<str:movie_id>', views.delete, name='delete')
+    path('erstellen/', views.create, name='create'),
+    path('bearbeiten/<str:movie_id>', views.edit, name='edit'),
+    path('löschen/<str:movie_id>', views.delete, name='delete')
 ]
