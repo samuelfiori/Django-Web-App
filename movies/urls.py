d334efb5f8ed84fe849cf297ff80cded4611e95c
samuelfiori <samuel.fiori1@gmx.de>
2018-02-18 14:05:27 +0000

    step_7_edit_movies_backend

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-    path('create/', views.create, name='create'),
+    path('erstelle/', views.create, name='create'),
     path('edit/<str:movie_id>', views.edit, name='edit'),
 ]
